f4399c66599bdf30c93f3efea6e38c1491b97607
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-04-08 15:11:12 -0700

    setup db (Mongo), created POST route

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+
+let db,
+    dbConnectionStr = 'mongodb+srv://demo:demo@cluster0.xcdy5.mongodb.net/todo?retryWrites=true&w=majority',
+    dbName = 'todo'
+
+MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
+    .then(client => {
+        console.log(`Hey, connected to ${dbName} database`)
+        db = client.db(dbName)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+//route that will hear the POST
+app.post('/createToDo', (req, res) => {
+    db.collection('todos').insertOne({todo: req.body.todoItem, completed: false})
+    .then(result => {
+        console.log('Todo item has been added')
+        res.redirect('/')
+    })
+    //console.log(req.body.todoItem)
+    
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running, you better catch it!')
+})
+
